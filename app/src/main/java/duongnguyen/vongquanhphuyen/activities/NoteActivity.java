42ca960269a7951c69ed3e73cd68777ddb8ea7e6
Duong Nguyen <nguyenthaiduong12112005@gmail.com>
2026-05-17 14:14:33 +0700

    thiết kế thêm chi tiết cho trang detaildes

---
+++ app/src/main/java/duongnguyen/vongquanhphuyen/activities/NoteActivity.java
@@ -0,0 +1,16 @@
+package duongnguyen.vongquanhphuyen.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import duongnguyen.vongquanhphuyen.R;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
